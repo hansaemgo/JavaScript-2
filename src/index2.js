fc679b446a1887865d7d62b15bdcaeb8a79261ef
hansaemgo <rhgkstoa7@gmail.com>
2019-12-31 16:01:29 +0900

    Truthy & Falsy
    falsy한 값

--- src/index2.js
+++ src/index2.js
@@ -1,6 +1,6 @@
 function print(person) {
- if (!person){
- // if (person === undefined || person === null) {
+  if (!person) {
+    // if (person === undefined || person === null) {
     return;
   }
   console.log(person.name);
@@ -12,17 +12,20 @@ const person = {
 
 print();
 
-console.log('falsy한 값')
+console.log("falsy한 값");
 console.log(!undefined);
 console.log(!null);
 console.log(!0);
-console.log(!'');
+console.log(!"");
 console.log(!NaN);
 console.log(!false);
 
+const value = { a: 1 };
+if (value) {
+  console.log("value 가 Truthy 하네요.");
+}
 
-const value = ;
-
-const truthy = !!value;
-
-
+const value2 = { a: 1 };
+const truthy = value2 ? true : false;
+//const value = { a: 1 };
+//const truthy = !!value;
