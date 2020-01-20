90d3a8dd1469ed4ff802d7e9039e21ddb1860a06
hansaemgo <rhgkstoa7@gmail.com>
2020-01-20 17:37:17 +0900

    function , arrow function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,87 @@
-//삼항 연산자의 사용법 => 조건 ? true일때 : false일때
+// (특정 값들의 합 구하기)
+const a = 1;
+const b = 2;
+const sum1 = a + b;
 
-const array = [1, 2];
-let text = "";
-if (array.length === 0) {
-  text = "배열이 비어있습니다.";
-} else {
-  text = "배열이 비어있지 않습니다.";
+
+// 위의 코드 함수로 만들기
+// function 함수이름(파라미터)
+function add (c, d) {
+  return c + d;
+  console.log('호출이 되지 않는 코드!');
 }
-console.log(text);
+const sum = add(5, 10);
+console.log(sum);
 
-const array2 = [];
-let text2 =
-  array2.length === 0 ? "배열이 비어있습니다" : "배열이 비어있지 않습니다";
-console.log(text2);
 
-// 이런 연산은 If문 쓰는 것이 좋음
-const condition1 = false;
-const condition2 = false;
+// name 파라미터를 넣으면 콘솔에 hello name! 출력
+function hello(name) {
+  console.log('hello ' + name + '!');
+}
+hello('Mark');
 
-const value = condition1 ? "와우!" : condition2 ? "blabla" : "foo";
 
-console.log(value);
+// 위의 코드 템플릿리터럴 사용
+function hello1(name) {
+  console.log(`hello ${name}!`);
+}
+hello1('멍멍이');
+
+
+// 다음과 같은 코드로도 사용 가능하지만 return뒤에 호출되지 않으므로 코드 길어짐
+function hello2(name) {
+  return `hello ${name}!`
+}
+const result = hello2('거북이');
+console.log(result);
+
+
+
+/*=================== 점수를 성적등급으로 변환하기 ============*/
+
+function getGrade(score) {
+  if ( score === 100 ){
+    return 'A+';
+  } else if ( score >= 90 ){
+    return 'A';
+  } else if ( score === 89 ){
+    return 'B+';
+  } else if ( score >= 80 ){
+    return 'B';
+  } else if ( score === 79 ){
+    return 'C+';
+  } else if ( score >= 70 ){
+    return 'C';
+  } else if ( score === 69 ){
+    return 'D+';
+  } else if ( score >= 60 ){
+    return 'D';
+  } else {
+    return 'F';
+  }  
+}
+
+const grade = getGrade(30);
+console.log(grade);
+
+
+/*======================= 화살표 함수 만들기 ====================*/
+// const add2 = (x, y) => {
+//   return x + y ;
+// }
+// console.log(add2(4, 3));
+
+ const add2 = (x, y) => x + y;
+ const sum2 = add2(10, 20);
+ console.log(sum2);
+
+const hello3 = (name) => {
+  console.log(`hello ${name}!`);
+}
+
+hello3('야옹이');
+
+
+
+
+
